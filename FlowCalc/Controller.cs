6455c1565502888d326fc90f5029a62412ad07ab
Elias Ruemmler <e.ruemmler@rc-art.de>
2020-05-15 02:03:31 +0200

    bugfix usings [ci]

--- FlowCalc/Controller.cs
+++ FlowCalc/Controller.cs
@@ -10,6 +10,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
