da46141f73e1854429018e336be6d4d42cdc323d
Elias Ruemmler <elias.ruemmler@de.bosch.com>
2021-03-30 22:12:37 +0200

    Add some numeric math

---
+++ FlowCalc/Mathematics/Polynom.cs
@@ -0,0 +1,63 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearRegression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowCalc.Mathematics
+{
+    public class Polynom
+    {
+        /// <summary>
+        /// Coefficients
+        /// Start with the offsetvalue of the polynom
+        /// </summary>
+        public double[] Coefficients { get; set; }
+
+
+        public Polynom(params double[] coefficients)
+        {
+            Coefficients = coefficients;
+        }
+
+        /// <summary>
+        /// Polynomial evaluation
+        /// </summary>
+        /// <param name="x">x value</param>
+        /// <returns>Calculated y value</returns>
+        public double Polyval(double x)
+        {
+            double result = 0;
+
+            for (int i = 0; i < Coefficients.Length; i++)
+                result += Coefficients[i] * Math.Pow(x, i);
+            return result;
+        }
+
+        /// <summary>
+        /// Polynomial curve fitting
+        /// </summary>
+        /// <param name="x">x values</param>
+        /// <param name="y">y values</param>
+        /// <param name="degree">fit degree</param>
+        /// <returns>Polynom with coefficients for a polynomial p(x) of degree n that is a best fit for the data in y.
+        /// The coefficients in p are in acscending powers (offset first), and the length of p is n+1</returns>
+        public static Polynom Polyfit(double[] x, double[] y, int degree)
+        {
+            if (x.Length != y.Length)
+                throw new ArgumentOutOfRangeException("Polyfit nicht möglich, da der X- und Y-Vektor verschiedene Längen haben!");
+
+            if (degree >= x.Length)
+                throw new ArgumentOutOfRangeException("Polyfit nicht möglich, da der angegeben Fit-Grad mit der Länge der Eingangsdaten nicht erreicht werden kann!");
+
+
+            var design = Matrix<double>.Build.Dense(x.Length, degree + 1, (i, j) => Math.Pow(x[i], j));
+            return new Polynom()
+            {
+                Coefficients = MultipleRegression.QR(design, Vector<double>.Build.Dense(y)).ToArray()
+            };
+        }
+    }
+}
---
+++ FlowCalc_Test/Polynom_Test.cs
@@ -0,0 +1,36 @@
+﻿using FlowCalc.Mathematics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FlowCalc_Test
+{
+    [TestClass]
+    public class Polynom_Test
+    {
+        [TestMethod]
+        public void Polyfit1()
+        {
+            var h = new double[7] { 0.4, 0.8, 1.15, 1.35, 1.45, 1.5, 1.52 };
+            var q = new double[7] { 6, 5, 4, 3, 2, 1, 0 };
+
+            var p = Polynom.Polyfit(q, h, 3);
+
+            //Calculated with Matlab
+            var expectedResult = new double[4] { 1.515, 0.009761904761905, -0.012857142857143, -0.003333333333333 };
+
+            for (int i = 0; i < expectedResult.Length; i++)
+                Assert.AreEqual(expectedResult[i], p.Coefficients[i], 1E-10);
+
+        }
+
+        [TestMethod]
+        public void Polyval1()
+        {
+            var p = new Polynom(1.515, 0.009761904761905, -0.012857142857143, -0.003333333333333);
+
+            var h = p.Polyval(3.5);
+
+            Assert.AreEqual(1.24875, h, 1E-10);
+        }
+    }
+}
